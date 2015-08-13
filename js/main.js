79f21219dba2e563c01f38d6e7221378fc02bfa1
shelune <em.hieunguyen@gmail.com>
2015-08-13 21:41:21 +0700

    Finishing the first page .. So much confusion

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*jslint browser: true*/
+/*jslint node: true*/
+/*global $, jQuery, alert*/
+"use strict";
+
+$(document).ready(function () {
+    var iPhone = $('.iPhone-wrapper'),
+        slideWrapper = $('.iPhone-slides'),
+        themeWrapper = $('themes-wrapper'),
+        themeIndex = 1,
+        colors = ["7db3b8", "df6e51", "005670", "87cbd8", "b9ce6c", "cc545a"];
+        // #7db3b8 - cloudy skyblue
+        // #df6e51 - cloudy tomato
+        // #005670 - dark turquoise
+        // #87cbd8 - light skyblue
+        // #b9ce6c - light pickled
+        // #cc545a - cloudy crimson
+
+
+
+    $('.fullpage').fullpage({
+
+    });
+
+});
